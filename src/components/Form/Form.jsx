f706605e8ab92f5067a47da7b2c445cff85191ec
sckarlet015 <sckarlet015@gmail.com>
2023-09-25 17:38:50 -0600

    form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,99 +1,91 @@
-//Bibliotecas
 import { useState } from "react";
-//Funciones
-import { validateRece } from "./validationsForm";
-//Estilos
+import { validateMail } from "./validationsForm";
 import style from "./Form.module.css";
+import axios from "axios";
 
-export default function Form({formRef}) {
-    const [seleccionados, setSeleccionados] = useState(0);
-    const [errors, setErrors] = useState({});
-    const [createRece, setCreateRece] = useState({
-        name: "",
-        image: "",
-        summary: "",
-        healthScore: "",
-        steps: "",
-        diets: [],
+export default function Form({ formRef }) {
+  const [errors, setErrors] = useState({});
+  const [createMail, setCreateMail] = useState({
+    name: "",
+    email: "",
+    asunto: "Vacio",
+    detalles: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setCreateMail((prevCreateMail) => ({
+      ...prevCreateMail,
+      [name]: value,
+    }));
+    setErrors((prevErrors) => validateMail({ ...createMail, [name]: value }));
+  };
+
+  const handleAsuntoChange = (e) => {
+    const { name, value } = e.target;
+    setCreateMail((prevCreateMail) => ({
+      ...prevCreateMail,
+      [name]: value,
+    }));
+  };
+
+  const handlePostRece = async () => {
+    const hasErrors = Object.values(errors).some((error) => error !== "");
+    if (hasErrors) {
+      alert("Lo sentimos, aún hay errores. Por favor, revisa los datos e inténtalo nuevamente.");
+      return;
+    } else {
+      try {
+          await axios.post("https://portafolioback-583e1a26d22c.herokuapp.com/send-email", createMail)
+          alert("Mensaje enviado")
+      } catch (error) {
+          alert(error)
+      }
+      resetForm();
+    }
+  };
+
+  const resetForm = () => {
+    setCreateMail({
+      name: "",
+      email: "",
+      asunto: "Vacio",
+      detalles: "",
     });
-    const initialState = {
-        name: "",
-        image: "",
-        summary: "",
-        healthScore: "",
-        steps: "",
-        diets: [],
-    };
-    const resetForm = () => {
-        setCreateRece(initialState);
-        setErrors({});
-        resetCheckboxes();
-    };
-    const resetCheckboxes = () => {
-        const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-        checkboxes.forEach((checkbox) => {
-            checkbox.checked = false;
-        });
-    };
-    const manejarCambio = (opcion) => {
-        if (seleccionados === 0) {
-            setCreateRece((prevCreateRece) => ({
-                ...prevCreateRece,
-                diets: [...prevCreateRece.diets, opcion],
-            }));
-            setErrors((prevErrors) =>
-                validateRece({ ...createRece, diets: opcion })
-            );
-        }
-    };
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setCreateRece((prevCreateRece) => ({
-            ...prevCreateRece,
-            [name]: value,
-        }));
-        setErrors((prevErrors) => validateRece({ ...createRece, [name]: value }));
-    };
-    const handlePostRece = async () => {
-        const hasErrors = Object.values(errors).some((error) => error !== "");
-        if (hasErrors) {
-            alert("Lo sentimos, aún hay errores. Por favor, revisa los datos e inténtalo nuevamente.");
-            return;
-        }
-       
-        resetForm();
-    };
-    return (
-        <div className={style.form}>
-            <h3 ref={formRef}>¿Como seria tu sitio web ideal?</h3>
-            <div>
-                <div className={style.conten}>
-                    <span className={style.title}>¿Cual es tu nombre? </span>
-                    <input className={style.inp} name="name" value={createRece.name} onChange={handleInputChange} placeholder=""></input>
-                    <p className={style.danger}>{errors.name}</p>
-                    <span className={style.title}>¿Como es tu correo? </span>
-                    <input className={style.inp} name="name" value={createRece.name} onChange={handleInputChange} placeholder=""></input>
-                    <span className={style.title}>¿Cual es el asunto? </span>
-                   <select name="asunto" id="" className={style.asuntoSelect}>
-                    <option className={style.asunto} value="pagina-web">Asunto</option>
-                    <option className={style.asunto} value="pagina-web">Crear sitio web</option>
-                    <option className={style.asunto} value="pagina-web">Oferta laboral</option>
-                    <option className={style.asunto} value="pagina-web">Colaboracion en un proyecto</option>
-                   </select>
-                    <span className={style.title}>¿Cuales son los detalles? </span>
-                    <textarea className={style.inp} name="summary" value={createRece.summary} onChange={handleInputChange} placeholder="" rows={9} cols={50}></textarea>
-                    <p className={style.danger}>{errors.summary}</p>
-                </div>
-            </div>
-            {Object.values(errors).some((error) => error !== "") ? (
-                <p className={style.danger}>
-                    Llene todos los campos.
-                </p>
-            ) : (
-                <button className={style.crear} onClick={handlePostRece}>
-                    Enviar
-                </button>
-            )}
+    setErrors({});
+  };
+
+  return (
+    <div className={style.form}>
+      <h3 ref={formRef}>¿Cómo sería tu sitio web ideal?</h3>
+      <div>
+        <div className={style.conten}>
+          <span className={style.title}>¿Cuál es tu nombre? </span>
+          <input className={style.inp} name="name" value={createMail.name} onChange={handleInputChange} />
+          <p className={style.danger}>{errors.name}</p>
+          <span className={style.title}>¿Cuál es tu correo? </span>
+          <input className={style.inp} name="email" value={createMail.email} onChange={handleInputChange} />
+          <p className={style.danger}>{errors.email}</p>
+          <span className={style.title}>¿Cuál es el asunto? </span>
+          <select name="asunto" value={createMail.asunto} onChange={handleAsuntoChange} className={style.asuntoSelect}>
+            <option value="Vacio">Asunto</option>
+            <option value="Crear sitio web">Crear sitio web</option>
+            <option value="Oferta laboral">Oferta laboral</option>
+            <option value="Colaboración en un proyecto">Colaboración en un proyecto</option>
+          </select>
+          <p className={style.danger}>{errors.asunto}</p>
+          <span className={style.title}>¿Cuáles son los detalles? </span>
+          <textarea className={style.inp} name="detalles" value={createMail.detalles} onChange={handleInputChange} rows={9} cols={50}></textarea>
+          <p className={style.danger}>{errors.detalles}</p>
         </div>
-    );
+      </div>
+      {Object.values(errors).some((error) => error !== "") ? (
+        <p className={style.danger}>Llene todos los campos.</p>
+      ) : (
+        <button className={style.crear} onClick={handlePostRece}>
+          Enviar
+        </button>
+      )}
+    </div>
+  );
 }
--- src/components/Form/validationsForm.js
+++ src/components/Form/validationsForm.js
@@ -1,29 +1,44 @@
 function contieneNumero(name) {
     return /\d/.test(name);
 }
+
 function contieneSimbolo(name) {
     return /[^\w\s]/.test(name);
 }
 
-function validateMail(mail){
-    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)
+function validateEmail(mail) {
+    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail);
 }
 
-
-export function validateRece(createRece) {
+export function validateMail(createMail) {
     const errors = {};
-    if (createRece.name.length === 0) {
-        errors.name = "El nombre no puede quedar vacio"
+
+    if (createMail?.name?.length === 0) {
+        errors.name = "El nombre no puede quedar vacío";
     }
-    if (contieneNumero(createRece.name)) {
-        errors.name = "No debe de incluir numeros"
+
+    if (contieneNumero(createMail?.name)) {
+        errors.name = "No debe incluir números";
     }
-    if (contieneSimbolo(createRece.name)) {
-        errors.name = "No debe de contener simbolos"
+
+    if (contieneSimbolo(createMail?.name)) {
+        errors.name = "No debe contener símbolos";
     }
-    
-    if (createRece.summary.length === 0) {
-        errors.summary = "El resumen no puede quedar vacio"
+
+    if (!validateEmail(createMail?.email)) {
+        errors.email = "Correo inválido";
+    }
+
+    if (createMail?.detalles?.length === 0) {
+        errors.detalles = 'El campo no debe quedar vacío';
+    }
+
+    if (createMail?.asunto?.length === 0) {
+        errors.asunto = 'Debe seleccionar un asunto';
+    }
+
+    if (createMail?.asunto === "Vacio") {
+        errors.asunto = 'Debe seleccionar un asunto';
     }
 
     return errors;
